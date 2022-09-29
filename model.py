484e4a263891b4fd266c3bee44205d196d7111a2
Bredskov <Nikolaj.bredskov@gmail.com>
2022-09-29 11:15:01 +0200

    finished wheel and Tank

--- model.py
+++ model.py
@@ -1,16 +1,42 @@
 from random import randint
 
+
+
+
 class Car(object):
-    pass
+
 
 class Wheel(object):
-    pass
+    def __init__(self):
+        self.orientation = randint(0, 360)
+
+    def rotate(self, revolutions):
+        degreesOfRotation = (360 * revolutions) %360
+        self.orientation = (self.orientation + degreesOfRotation % 360)
 
 class Engine(object):
-    pass
+
 
 class Gearbox(object):
-    pass
+
 
 class Tank(object):
-    pass
+
+    def __init__(self):
+        self.capacity = 100
+        self.contents = 100
+    def refuel(self):
+        self.contents = self.capacity
+    
+    def remove(self, amount):
+        self.contents -= amount
+        if self.contents <0:
+            self.contents = 0
+
+
+
+
+
+
+
+
